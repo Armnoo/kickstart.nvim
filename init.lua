5a5f4d2de0b072be2b5ba97ccff92ea4c5d42e52
Anon <37373373+Armnoo@users.noreply.github.com>
2025-07-14 03:40:19 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1014,7 +1014,6 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+-- Personal user config
+require('user.config')
