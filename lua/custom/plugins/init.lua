13cee52c5df9db9ce6564446bf42b3e48d240b30
Anon <37373373+Armnoo@users.noreply.github.com>
2025-07-13 22:23:19 -0500

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,10 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- User config
+  vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+  vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+  -- vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+  -- vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+}
 
--- User config
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
--- vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
--- vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
